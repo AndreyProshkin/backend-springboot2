45566447f294e7eb3e96d8a4cc661b4c0ed1f599
AndreyProshkin <andrey_pro@inbox.ru>
2020-07-09 12:07:55 +0300

    test

---
+++ src/test/java/ru/javabegin/tasklist2/backendspringboot2/BackendSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.tasklist2.backendspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
