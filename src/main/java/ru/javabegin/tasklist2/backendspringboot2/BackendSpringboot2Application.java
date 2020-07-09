54de18075310be9c7627f677bee3a5cc4ffde3e0
AndreyProshkin <andrey_pro@inbox.ru>
2020-07-09 10:48:34 +0300

    test

---
+++ src/main/java/ru/javabegin/tasklist2/backendspringboot2/BackendSpringboot2Application.java
@@ -0,0 +1,13 @@
+package ru.javabegin.tasklist2.backendspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringboot2Application.class, args);
+    }
+
+}
